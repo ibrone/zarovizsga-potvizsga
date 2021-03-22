b1e33387b5f40af7f096f45de2cf6c9a15a47920
Norbi <ibrone@gmail.com>
2021-03-22 18:55:37 +0100

    záró pót

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/peoplesql/PeopleDao.java
@@ -0,0 +1,25 @@
+//package hu.nive.ujratervezes.zarovizsga.peoplesql;
+//
+//import java.sql.Connection;
+//
+//import java.sql.DatabaseMetaData;
+//import java.sql.PreparedStatement;
+//import java.sql.SQLException;
+//
+//public class PeopleDao {
+//
+//
+//    public PeopleDao() {
+//        DatabaseMetaData dataSource;
+//        try (Connection conn = dataSource.getConnection())
+//        PreparedStatement stmt = conn.preparedStatement();
+//        )
+//            stmt.setString(1, name);
+//            stmt.execute();
+//        } catch (SQLException){
+//    throw new IllegalArgumentException("Error",sqle);
+//
+//
+//        }
+//    }
+//}
